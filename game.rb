dfb6a19c5669d7f1c88c3196150cd3bf28a3b931
Owner <Owner>
2016-04-23 15:03:11 +0100

    seperated out the classes from the game file and added require statements

--- game.rb
+++ game.rb
@@ -1,53 +1,6 @@
-class Card
-	attr_accessor :rank, :suit
-	def initialize(rank, suit)
-		@rank = rank
-		@suit = suit
-	end
-end
-#generate a full deck of cards
-class Deck
-	attr_accessor :deck, :hand
-	def initialize
-		@deck = []
-		faces = %w(J Q K A)
-		ranks = %W(2 3 4 5 6 7 8 9 10)
-		suits = %w(Hearts Spades Diamonds Clubs)
-		suits.each do |suit|
-			ranks.each do |i|
-				@deck << Card.new(i, suit)
-			end
-			faces.each do |i|
-				@deck << Card.new(i, suit)
-			end
-		end
-	end
-	#returns the current state of the deck
-	def print_deck
-		@deck.each do |card| 
-			puts "#{card.rank} #{card.suit}"
-		end
-	end
-	#shuffles deck
-	def shuffle_cards
-		@deck.shuffle!
-	end
-end
-
-class Player
-	attr_accessor :name, :hand
-	def initialize(name)
-		@name = name
-		@hand = []
-	end
-end
-
-class Dealer
-	attr_accessor :hand
-	def initialize
-		@hand = []
-	end
-end
+require './dealer.rb'
+require './player.rb'
+require './cards_and_deck.rb'
 
 class Game
 	attr_accessor :rank, :suit, :hand
